674480f902d4b98338bb6a6a4d3db3a87e882ef5
TimDaiber <tdaiber90@hotmail.com>
2016-09-15 17:11:13 +0100

    Probelm1 Finished

---
+++ Problem1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+    
+    var sum int;
+
+    for i := 0;i  <= 100 ; i++ {
+        if i % 3 ==0 {
+            sum = sum + i;
+        }
+        if i % 5 == 0{
+            sum = sum +i;
+        }
+    }
+
+
+    fmt.Println(sum);
+
+}
--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## Terminal Commands for GO
 
 -go run filename.go
+
+### Problem1.go
+
+The natural numbers below 10 that are multiples of 3 or 5 are: 3, 5, 6 and 9. The sum
+of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000
+
+#### Resources
+
